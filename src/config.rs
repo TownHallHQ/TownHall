0677bb1f82f4c0568693847e76b9fbf1dffbb434
Michael Liendo <michael.m.liendo.r@gmail.com>
2022-09-14 11:40:07 -0400

    fix: editable port

--- src/config.rs
+++ src/config.rs
@@ -2,13 +2,13 @@ use std::env;
 
 pub struct Config {
     pub database_url: String,
-    pub server_port: Number,
+    pub server_port: u16,
 }
 
 impl Config {
     pub fn new() -> Self {
         let database_url = env::var("DATABASE_URL").unwrap();
-        let server_port = env::var("PORT").unwrap();
+        let server_port = env::var("PORT").unwrap().parse::<u16>().unwrap();
 
         Self { database_url, server_port }
     }

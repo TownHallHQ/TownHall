6acbed5b8236b57a9ed4f04d72f631b1456e81a0
Esteban Borai <estebanborai@gmail.com>
2022-09-16 14:23:21 -0300

    feat: environment variable error handling (#5)

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::str::FromStr;
 
 pub struct Config {
     pub cors_allow_origin: String,
@@ -8,14 +9,28 @@ pub struct Config {
 
 impl Config {
     pub fn new() -> Self {
-        let cors_allow_origin = env::var("CORS_ALLOW_ORIGIN").unwrap();
-        let database_url = env::var("DATABASE_URL").unwrap();
-        let server_port = env::var("PORT").unwrap().parse::<u16>().unwrap();
+        let cors_allow_origin = Config::env_var::<String>("CORS_ALLOW_ORIGIN");
+        let database_url = Config::env_var::<String>("DATABASE_URL");
+        let server_port = Config::env_var::<u16>("PORT");
 
         Self {
             cors_allow_origin,
             database_url,
             server_port,
         }
+    }
+
+    fn env_var<T: FromStr>(key: &str) -> T {
+        let value =
+            env::var(key).unwrap_or_else(|_| panic!("Missing environment variable: {}", key));
+
+        if let Ok(parsed) = str::parse::<T>(&value) {
+            return parsed;
+        }
+
+        panic!(
+            "Failed to parse environment variable value from key: {}",
+            key
+        );
     }
 }

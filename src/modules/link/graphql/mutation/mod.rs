17e1982c907b1b0eb7379b7e584627a3c2d4306d
Dave136 <davejs136@gmail.com>
2023-02-08 14:50:51 -0400

    feat: execute link create mutation

--- src/modules/link/graphql/mutation/mod.rs
+++ src/modules/link/graphql/mutation/mod.rs
@@ -1,13 +1,17 @@
-use async_graphql::Object;
+mod link_create;
+
+use async_graphql::{Context, Object, Result};
+
+use link_create::LinkCreate;
+
+use self::link_create::LinkCreateInput;
 
 #[derive(Debug, Default)]
 pub struct LinkMutationRoot;
 
 #[Object]
 impl LinkMutationRoot {
-    async fn link_create(&self) -> bool {
-        println!("LinkMutationRoot::link_create");
-
-        true
+    async fn link_create(&self, ctx: &Context<'_>, input: LinkCreateInput) -> Result<LinkCreate> {
+        LinkCreate::exec(ctx, input).await
     }
 }

5bde47a7005d97eeedd88dcf07f2be0b84f87fed
Esteban Borai <estebanborai@gmail.com>
2023-02-12 15:09:19 -0400

    fix: remove store from services creation

--- src/context/mod.rs
+++ src/context/mod.rs
@@ -27,7 +27,7 @@ impl Context {
     pub async fn new(config: &Config) -> Result<Self> {
         let db_path = PathBuf::from(config.database_path.clone());
         let store = Arc::new(Store::new(db_path));
-        let services = Arc::new(Services::new(store.clone(), config));
+        let services = Arc::new(Services::new(config));
         let repositories = Arc::new(Repositories::new(store));
 
         Ok(Self {
--- src/context/services.rs
+++ src/context/services.rs
@@ -2,14 +2,14 @@ use std::sync::Arc;
 
 use crate::modules::auth::service::AuthService;
 
-use super::{Config, Store};
+use super::Config;
 
 pub struct Services {
     pub auth: Arc<AuthService>,
 }
 
 impl Services {
-    pub fn new(_store: Arc<Store>, config: &Config) -> Self {
+    pub fn new(config: &Config) -> Self {
         let auth = AuthService::new(config.jwt_secret.clone());
 
         Self {

dcd98086e716210d0302c0ea30720b5d0f55ba35
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-06-26 16:48:30 -0400

    test: display errors test

--- src/test/src/modules/user/user_register.rs
+++ src/test/src/modules/user/user_register.rs
@@ -30,14 +30,14 @@ async fn creates_a_new_user() {
     let result = schema
         .execute(
             Request::new(mutation).variables(Variables::from_json(json!({
-                "payload": {
-                  "name": "John",
-                  "surname": "Appleseed",
-                  "username": "john_appleseed",
-                  "password": "Root$1234",
-                  "email": "john_appleseed@whizzes.io",
-            }
-              }))),
+                    "payload": {
+                        "name": "John",
+                        "surname": "Appleseed",
+                        "username": "john_appleseed",
+                        "password": "Root$1234",
+                        "email": "john_appleseed@whizzes.io",
+                }
+            }))),
         )
         .await;
     let data = result.data.into_json().unwrap();
@@ -49,4 +49,126 @@ async fn creates_a_new_user() {
     assert_eq!(user_register_user["username"], "john_appleseed");
     assert!(user_register_user["createdAt"].is_string());
     assert!(user_register_user["updatedAt"].is_string());
+}
+
+#[tokio::test]
+async fn creates_a_new_user_with_existing_email() {
+    let test_util = TestUtil::new_cleared().await;
+    let (_, schema) = test_util.parts();
+    let mutation: &str = "
+        mutation RegisterUser($payload: UserRegisterInput!) {
+            userRegister(input: $payload) {
+                user {
+                    id
+                    name
+                    surname
+                    username
+                    email
+                    createdAt
+                    updatedAt
+                }
+                error {
+                    code
+                    message
+                }
+            }
+        }
+    ";
+
+    let _ = schema
+        .execute(
+            Request::new(mutation).variables(Variables::from_json(json!({
+                "payload": {
+                    "name": "John",
+                    "surname": "Doe",
+                    "username": "johndoe",
+                    "password": "Root$1234",
+                    "email": "john_appleseed@whizzes.io",
+                }
+            }))),
+        )
+        .await;
+
+    let result = schema
+        .execute(
+            Request::new(mutation).variables(Variables::from_json(json!({
+                "payload": {
+                    "name": "Jane",
+                    "surname": "Smith",
+                    "username": "janesmith",
+                    "password": "Root$5678",
+                    "email": "john_appleseed@whizzes.io",
+                }
+            }))),
+        )
+        .await;
+    let data = result.data.into_json().unwrap();
+    let user_register_error = &data["userRegister"]["error"];
+
+    assert_eq!(user_register_error["code"], "INTERNAL");
+    assert_eq!(
+        user_register_error["message"],
+        "An error ocurred: The email john_appleseed@whizzes.io is already taken"
+    );
+}
+
+#[tokio::test]
+async fn creates_a_new_user_with_existing_username() {
+    let test_util = TestUtil::new_cleared().await;
+    let (_, schema) = test_util.parts();
+    let mutation: &str = "
+        mutation RegisterUser($payload: UserRegisterInput!) {
+            userRegister(input: $payload) {
+                user {
+                    id
+                    name
+                    surname
+                    username
+                    email
+                    createdAt
+                    updatedAt
+                }
+                error {
+                    code
+                    message
+                }
+            }
+        }
+    ";
+
+    let _ = schema
+        .execute(
+            Request::new(mutation).variables(Variables::from_json(json!({
+                "payload": {
+                    "name": "John",
+                    "surname": "Doe",
+                    "username": "john_appleseed",
+                    "password": "Root$1234",
+                    "email": "john.doe@example.com",
+                }
+            }))),
+        )
+        .await;
+
+    let result = schema
+        .execute(
+            Request::new(mutation).variables(Variables::from_json(json!({
+                "payload": {
+                    "name": "Jane",
+                    "surname": "Smith",
+                    "username": "john_appleseed",
+                    "password": "Root$5678",
+                    "email": "jane.smith@example.com",
+                }
+            }))),
+        )
+        .await;
+    let data = result.data.into_json().unwrap();
+    let user_register_error = &data["userRegister"]["error"];
+
+    assert_eq!(user_register_error["code"], "INTERNAL");
+    assert_eq!(
+        user_register_error["message"],
+        "An error ocurred: The username john_appleseed is already taken"
+    );
 }

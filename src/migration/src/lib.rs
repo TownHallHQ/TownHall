d584db3347f6ad5fe945f7dd7668b1647765793a
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-07-04 21:22:14 -0400

    chore: remove chats

--- src/migration/src/lib.rs
+++ src/migration/src/lib.rs
@@ -1,9 +1,6 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20230408_000001_create_table_user;
-mod m20230621_000001_create_table_chat;
-mod m20230621_000002_create_table_message;
-mod m20230621_000003_create_table_user_chats;
 mod m20230704_000001_create_table_post;
 
 pub const PXID_LENGTH: u32 = 30;
@@ -15,9 +12,6 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m20230408_000001_create_table_user::Migration),
-            Box::new(m20230621_000001_create_table_chat::Migration),
-            Box::new(m20230621_000002_create_table_message::Migration),
-            Box::new(m20230621_000003_create_table_user_chats::Migration),
             Box::new(m20230704_000001_create_table_post::Migration),
         ]
     }

76f2c49d798e5538557dcb109341b96816db4431
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-07-06 18:26:58 -0400

    feat: login page

--- client/src/routes/(auth)/+layout.svelte
+++ client/src/routes/(auth)/+layout.svelte
@@ -1,5 +1,9 @@
-<div class="flex min-h-screen">
-  <div class="flex flex-row w-full">
+<script>
+  import Card from '$lib/components/Card.svelte';
+</script>
+
+<div class="flex bg-gray-900 min-h-screen justify-center items-center">
+  <Card class="h-max w-full mx-2 md:w-1/2 xl:w-1/4">
     <slot />
-  </div>
+  </Card>
 </div>

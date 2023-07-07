c981ffd3c8cf1134b00c32334eed4a0e9b9403a0
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-07-07 15:38:21 -0400

    feat: unsplash daily (#75)

--- client/src/routes/(auth)/+layout.svelte
+++ client/src/routes/(auth)/+layout.svelte
@@ -1,9 +1,41 @@
-<script>
+<script lang="ts">
+  import { onMount } from 'svelte';
   import Card from '$lib/components/Card.svelte';
+  import type { Unsplash } from '../api/unsplash/+server';
+
+  let cover: Unsplash;
+
+  onMount(async () => {
+    const response = await fetch('/api/unsplash');
+    const data = await response.json();
+    cover = data.data;
+
+    document.documentElement.style.setProperty(
+      '--cover-image',
+      `url('${cover?.url}')`
+    );
+  });
 </script>
 
-<div class="flex bg-gray-900 min-h-screen justify-center items-center">
+<div
+  class="flex bg-gray-900 min-h-screen justify-center items-center bg-cover bg-no-repeat"
+  style="background-image: var(--cover-image);"
+>
   <Card class="h-max w-full mx-2 md:w-1/2 xl:w-1/4">
     <slot />
   </Card>
+  <small class="text-sm text-white fixed bottom-4 right-4"
+    >Photo by <a
+      class="underline font-semibold"
+      href={`https://unsplash.com/@${cover?.author?.username}?utm_source=gabble&utm_medium=referral`}
+      >{cover?.author?.name || ''}</a
+    >
+    on
+    <a
+      class="underline font-medium"
+      href="https://unsplash.com/?utm_source=gabble&utm_medium=referral"
+    >
+      Unsplash</a
+    ></small
+  >
 </div>
---
+++ client/src/routes/api/unsplash/+server.ts
@@ -0,0 +1,91 @@
+import { createApi } from 'unsplash-js';
+
+import { UNSPLASH_ACCESS_KEY } from '$env/static/private';
+
+import type { RequestEvent } from './$types';
+
+export type Unsplash = {
+  url: string;
+  author: {
+    id: string;
+    name: string;
+    username: string;
+    url: string;
+    avatar: string;
+  };
+};
+
+export async function GET(event: RequestEvent) {
+  const UNSPLASH_DAILY_BACKGROUND = event.cookies.get(
+    'UNSPLASH_DAILY_BACKGROUND'
+  );
+
+  if (UNSPLASH_DAILY_BACKGROUND) {
+    return new Response(
+      JSON.stringify({
+        statusCode: 200,
+        data: JSON.parse(UNSPLASH_DAILY_BACKGROUND),
+        error: null,
+        success: true,
+      })
+    );
+  }
+
+  const unsplash = createApi({
+    accessKey: UNSPLASH_ACCESS_KEY || '',
+  });
+
+  const result = await unsplash.photos.getRandom({
+    query: 'night lights',
+  });
+
+  if (result.status === 200) {
+    const image = Array.isArray(result?.response)
+      ? result?.response[0]
+      : result?.response;
+
+    if (image) {
+      const today = new Date();
+      today.setDate(today.getDate() + 1);
+
+      const response = {
+        url: image.urls?.regular,
+        author: {
+          id: image.user?.id,
+          username: image.user?.username,
+          name: image.user?.name,
+          url: image.user?.portfolio_url,
+          avatar: image.user?.profile_image?.medium,
+        },
+      };
+
+      event.cookies.set('UNSPLASH_DAILY_BACKGROUND', JSON.stringify(response), {
+        expires: today,
+        path: '/',
+        httpOnly: true,
+        sameSite: 'strict',
+        secure: process.env.NODE_ENV === 'production',
+      });
+
+      return new Response(
+        JSON.stringify({
+          statusCode: 200,
+          data: response,
+          success: true,
+          error: null,
+        })
+      );
+    }
+  }
+
+  return new Response(
+    JSON.stringify({
+      statusCode: result.status,
+      error: {
+        message: result.errors?.[0] || 'An error ocurred fetching the resource',
+      },
+      data: null,
+      success: false,
+    })
+  );
+}

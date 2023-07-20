879eda6b1fa97d116cbb1ce5e418960e163c3745
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-07-20 18:46:10 -0400

    feat: post creation box

--- client/src/routes/(app)/+page.svelte
+++ client/src/routes/(app)/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import { page } from '$app/stores';
+  import PostBox from './components/PostBox.svelte';
 </script>
 
-<div class="px-4 pt-6 space-y-6">
+<div class="px-4 pt-6 space-y-6 sm:w-3/3 lg:w-1/2">
   Welcome back {$page.data.user.name}!
+  <PostBox />
 </div>

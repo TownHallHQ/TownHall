67c51c4b02c23c8f71c1d1100d091a0c6e731c76
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-07-20 23:13:17 -0400

    fix: mobile box width
    Co-authored-by: Esteban Borai <estebanborai@gmail.com>

--- client/src/routes/(app)/+page.svelte
+++ client/src/routes/(app)/+page.svelte
@@ -3,7 +3,7 @@
   import PostBox from './components/PostBox.svelte';
 </script>
 
-<div class="px-4 pt-6 space-y-6 sm:w-3/3 lg:w-1/2">
+<div class="px-4 pt-6 space-y-6 sm:w-full lg:w-1/2">
   Welcome back {$page.data.user.name}!
   <PostBox />
 </div>

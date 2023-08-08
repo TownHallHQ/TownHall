69f41a378ff9e006cc7efa9cf6fa8e83cbde22d4
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-08-08 09:15:55 -0400

    feat: svelte placeholder for loader on feed (#126)

--- client/src/routes/(app)/components/Feed.svelte
+++ client/src/routes/(app)/components/Feed.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
   import { onMount } from 'svelte';
+  import Placeholder from '@whizzes/svelte-placeholder';
 
   import intersectionObserver from '$lib/actions/intersection-observer';
   import Post from './Post.svelte';
+  import Card from '$lib/components/Card.svelte';
 
   import type { PostConnection, Post as PostType } from '$lib/graphql/schema';
 
   let posts: PostType[] = [];
-  let loading = false;
+  let loadingPosts = false;
   let lastPostId: string = '';
   let noMorePosts: boolean = false;
 
@@ -17,7 +19,8 @@
 
   async function fetchData() {
     try {
-      loading = true;
+      loadingPosts = true;
+
       const response = await fetch(`/get-posts?first=20&after=${lastPostId}`);
       const data: PostConnection = await response.json();
 
@@ -28,36 +31,50 @@
       } else {
         noMorePosts = true;
       }
-      loading = false;
+      loadingPosts = false;
     } catch (error) {
+      loadingPosts = false;
       console.log(error);
     }
   }
 
-  const load = () => {
-    fetchData();
-  };
-
   let elementRef: Element;
   $: {
     if (elementRef) {
-      intersectionObserver({ fetch: load, element: elementRef });
+      intersectionObserver({ fetch: fetchData, element: elementRef });
     }
   }
 </script>
 
-{#each posts as post}
-  <Post {post} />
-{/each}
+<ul class="space-y-6">
+  {#each posts as post}
+    <li>
+      <Post {post} />
+    </li>
+  {/each}
 
-<div class="mb-96">
-  {#if loading}
-    <span class="mb-20">Loading...</span>
+  {#if loadingPosts}
+    {#each Array.from({ length: 6 }) as _}
+      <li>
+        <Card class="h-[160px]">
+          <Placeholder width="100%">
+            <rect x="2" y="2" rx="8" ry="8" width="63" height="62" />
+            <rect x="75" y="3" rx="8" ry="8" width="149" height="22" />
+            <rect x="77" y="35" rx="8" ry="8" width="110" height="19" />
+            <rect x="2" y="72" rx="8" ry="8" width="130" height="25" />
+            <rect x="2" y="110" rx="8" ry="8" width="394" height="19" />
+          </Placeholder>
+        </Card>
+      </li>
+    {/each}
   {/if}
-  {#if loading === false && !noMorePosts}
-    <span class="mb-20" bind:this={elementRef}>Loading</span>
+  {#if loadingPosts === false && !noMorePosts}
+    <span class="mb-20" bind:this={elementRef}> 👀 </span>
   {/if}
   {#if noMorePosts}
-    <span>No more posts</span>
+    <li class="text-gray-600 flex flex-col justify-center items-center">
+      <figure class="text-[60px]">😵</figure>
+      <strong class="text-xl">Looking empty...</strong>
+    </li>
   {/if}
-</div>
+</ul>

abe6152cc4b5ce134089ec661f723ebb20d28fd3
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-08-10 08:07:14 -0400

    chore: change layout app

--- client/src/routes/(app)/+page.svelte
+++ client/src/routes/(app)/+page.svelte
@@ -2,7 +2,10 @@
   import { page } from '$app/stores';
   import Feed from './components/Feed.svelte';
   import PostBox from './components/PostBox.svelte';
+  import Sidebar from './components/Sidebar.svelte';
 </script>
+
+<Sidebar />
 
 <div class="px-4 pt-6 space-y-6 sm:w-full lg:w-1/2">
   Welcome back {$page.data.user.name}!

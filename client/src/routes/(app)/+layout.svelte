0483bb372ddeef8b5b739313a09b26a06a6cf5d5
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-08-11 20:55:14 -0400

    chore: move sidebar to the only use in the main page (#131)

--- client/src/routes/(app)/+layout.svelte
+++ client/src/routes/(app)/+layout.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
   import Navbar from './components/Navbar.svelte';
-  import Sidebar from './components/Sidebar.svelte';
 </script>
 
 <div>
   <Navbar />
   <div class="flex pt-16 overflow-hidden bg-gray-50 dark:bg-gray-900">
-    <Sidebar />
     <div
       id="main-content"
-      class="w-full min-h-[calc(100vh-64px)] overflow-y-auto bg-gray-50"
+      class="flex w-full min-h-[calc(100vh-64px)] overflow-y-auto bg-gray-50"
     >
       <slot />
     </div>

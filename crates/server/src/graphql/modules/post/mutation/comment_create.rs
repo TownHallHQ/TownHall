5ffd713ea9b81b3bcfe5fd8356729c0e0afa40d7
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-09-09 16:46:44 -0400

    chore: solve comments

--- crates/server/src/graphql/modules/post/mutation/comment_create.rs
+++ crates/server/src/graphql/modules/post/mutation/comment_create.rs
@@ -1,14 +1,10 @@
+use crate::context::SharedContext;
+use crate::graphql::modules::post::types::{Post, PostError};
+use crate::services::auth::Token;
 use async_graphql::{Context, InputObject, Result, SimpleObject};
 use playa::post::service::CreatePostDto;
 use pxid::graphql::Pxid;
 use serde::{Deserialize, Serialize};
-
-use crate::{
-    context::SharedContext,
-    graphql::modules::post::types::{Post, PostError},
-    services::auth::Token,
-};
-
 #[derive(Debug, InputObject)]
 pub struct CommentCreateInput {
     pub content: String,
@@ -25,9 +21,7 @@ impl CommentCreate {
     pub async fn exec(ctx: &Context<'_>, input: CommentCreateInput) -> Result<Self> {
         let context = ctx.data_unchecked::<SharedContext>();
         let token = ctx.data_unchecked::<Token>();
-
-        let claims = context.services.auth.verify_token(token).unwrap();
-
+        let claims = context.services.auth.verify_token(token)?;
         let dto = CreatePostDto {
             author_id: claims.uid,
             parent_id: Some(input.parent_id.into_inner()),

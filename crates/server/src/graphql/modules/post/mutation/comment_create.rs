16151e0f3069d527ebc4417639176eaf4801c86c
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-09-09 15:58:50 -0400

    feat: create comments

---
+++ crates/server/src/graphql/modules/post/mutation/comment_create.rs
@@ -0,0 +1,52 @@
+use async_graphql::{Context, InputObject, Result, SimpleObject};
+use playa::post::service::CreatePostDto;
+use pxid::graphql::Pxid;
+use serde::{Deserialize, Serialize};
+
+use crate::{
+    context::SharedContext,
+    graphql::modules::post::types::{Post, PostError},
+    services::auth::Token,
+};
+
+#[derive(Debug, InputObject)]
+pub struct CommentCreateInput {
+    pub content: String,
+    pub parent_id: Pxid,
+}
+
+#[derive(Debug, Deserialize, Serialize, SimpleObject)]
+pub struct CommentCreate {
+    comment: Option<Post>,
+    error: Option<PostError>,
+}
+
+impl CommentCreate {
+    pub async fn exec(ctx: &Context<'_>, input: CommentCreateInput) -> Result<Self> {
+        let context = ctx.data_unchecked::<SharedContext>();
+        let token = ctx.data_unchecked::<Token>();
+
+        let claims = context.services.auth.verify_token(token).unwrap();
+
+        let dto = CreatePostDto {
+            author_id: claims.uid,
+            parent_id: Some(input.parent_id.into_inner()),
+            title: "".to_string(),
+            content: Some(input.content),
+        };
+
+        match context.services.post.create(dto).await {
+            Ok(post) => Ok(Self {
+                comment: Some(Post::from(post)),
+                error: None,
+            }),
+            Err(err) => Ok(Self {
+                comment: None,
+                error: Some(PostError {
+                    code: crate::graphql::modules::post::types::PostErrorCode::Unknown,
+                    message: format!("An error ocurred: {err}"),
+                }),
+            }),
+        }
+    }
+}
--- crates/server/src/graphql/modules/post/mutation/mod.rs
+++ crates/server/src/graphql/modules/post/mutation/mod.rs
@@ -1,3 +1,4 @@
+mod comment_create;
 mod post_create;
 
 use async_graphql::{Context, Object, Result};
@@ -15,5 +16,15 @@ impl PostMutationRoot {
     #[graphql(guard = "AuthenticationGuard::new()")]
     async fn post_create(&self, ctx: &Context<'_>, input: PostCreateInput) -> Result<PostCreate> {
         PostCreate::exec(ctx, input).await
+    }
+
+    /// Creates a comment authored by the user identified by the provided token
+    #[graphql(guard = "AuthenticationGuard::new()")]
+    async fn comment_create(
+        &self,
+        ctx: &Context<'_>,
+        input: comment_create::CommentCreateInput,
+    ) -> Result<comment_create::CommentCreate> {
+        comment_create::CommentCreate::exec(ctx, input).await
     }
 }

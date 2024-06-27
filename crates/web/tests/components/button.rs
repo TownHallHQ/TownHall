fbc0bb6cda8eb4606cec732c67ffdee59f710a62
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-06-27 09:25:21 -0400

    feat: add custom class name to button component

--- crates/web/tests/components/button.rs
+++ crates/web/tests/components/button.rs
@@ -32,7 +32,43 @@ fn default_text_button_class_names_integrity() {
         "font-semibold",
         "text-sm",
         "btn-text",
-        "bg-transparent"
+        "bg-transparent",
+    ];
+
+    let have_class_names = button_el.get_attribute("class").unwrap();
+
+    for class_name in want_class_names.iter() {
+        assert!(have_class_names.contains(class_name));
+    }
+}
+
+#[wasm_bindgen_test]
+fn default_text_button_custom_class_names_integrity() {
+    let document = leptos::document();
+    let test_wrapper = document.create_element("div").unwrap();
+    let _ = document.body().unwrap().append_child(&test_wrapper);
+
+    mount_to(
+        test_wrapper.clone().unchecked_into(),
+        || view! { <Button id="btn-text" class="custom-class" variant={ButtonVariant::Text}>"Click Me!"</Button> },
+    );
+
+    let button_el = test_wrapper
+        .query_selector("#btn-text")
+        .expect("Failed to access document")
+        .expect("Button Element not found")
+        .unchecked_into::<web_sys::HtmlButtonElement>();
+
+    let want_class_names = vec![
+        "custom-class",
+        "px-4",
+        "py-2",
+        "rounded-md",
+        "cursor-pointer",
+        "font-semibold",
+        "text-sm",
+        "btn-text",
+        "bg-transparent",
     ];
 
     let have_class_names = button_el.get_attribute("class").unwrap();

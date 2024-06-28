fbe07a6f42a9316d4c329956c3fc94b439719eef
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-06-27 21:45:04 -0400

    feat: add sign up

--- crates/web/src/views/login.rs
+++ crates/web/src/views/login.rs
@@ -7,15 +7,16 @@ use crate::components::button::{Button, ButtonVariant};
 pub fn Login() -> impl IntoView {
     view! {
       <div class="min-h-screen relative flex justify-center items-center bg-no-repeat bg-cover bg-[url('https://images.unsplash.com/photo-1580192985016-7e15ef081dd8?q=80&w=1961&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
-          <div class="absolute bg-black rounded-[50%] w-full h-full blur-[100rem]"></div>
+          <div class="absolute bg-black rounded-[50%] w-full h-full blur-[23rem]"></div>
           <div class="flex justify-center items-center z-20">
-            <div>
-              <h1 class="text-5xl text-center font-bold text-white">TownHall</h1>
-              <form class="mt-14 w-96">
+            <div class="w-full">
+              <h1 class="text-6xl text-center font-bold text-white mb-16">TownHall</h1>
+              <form class="w-96">
                 <TextField name="email" placeholder="Email" />
                 <TextField name="password" r#type=TextFieldType::Password placeholder="Password" />
-                <Button class="w-full my-3" variant={ButtonVariant::Contained}>Log in</Button>
+                <Button class="w-full mt-3" variant={ButtonVariant::Contained}>Log in</Button>
               </form>
+              <div class="text-center w-full text-white mt-3">{"Don't have an account? "}<a class="underline" href="/signup">Sign up!</a></div>
             </div>
         </div>
       </div>

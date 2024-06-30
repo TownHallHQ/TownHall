c7f6e9ebcf11a0ab8e19009dc4c0283c12d88e52
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-06-29 22:54:26 -0400

    feat: change background color

--- crates/web/src/views/login.rs
+++ crates/web/src/views/login.rs
@@ -6,7 +6,7 @@ use crate::components::button::{Button, ButtonVariant};
 #[component]
 pub fn Login() -> impl IntoView {
     view! {
-      <div class="min-h-screen relative flex justify-center items-center bg-no-repeat bg-cover bg-[url('https://images.unsplash.com/photo-1580192985016-7e15ef081dd8?q=80&w=1961&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
+      <div class="min-h-screen relative flex justify-center items-center bg-no-repeat bg-cover bg-slate-800 bg-[url('https://images.unsplash.com/photo-1580192985016-7e15ef081dd8?q=80&w=1961&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
           <div class="absolute bg-black rounded-[50%] w-full h-full blur-[23rem]"></div>
           <div class="flex justify-center items-center z-20">
             <div class="w-full">

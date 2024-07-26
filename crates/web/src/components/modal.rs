f7aa58b2f9e9e5ce674adce7d33845c1c73bbec1
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-07-26 12:45:05 -0400

    chore: format code

--- crates/web/src/components/modal.rs
+++ crates/web/src/components/modal.rs
@@ -12,17 +12,12 @@ pub fn Modal(#[prop(into)] modal_status: RwSignal<bool>, children: ChildrenFn) -
     on_click_outside(modal_ref, move |_event| modal_status.set(false));
 
     view! {
-        <Suspense
-        fallback=|| ()
-    >
-        <Show when=move || modal_status.get()  fallback=move || ()>
-          <div class="fixed right-0 top-0 z-10 flex h-screen w-screen flex-col items-center justify-center drop-shadow-lg backdrop-blur-[2px] bg-[#00000055]">
-            <div node_ref=modal_ref>
-            {children.with_value(|children| children())}
-            </div>
-          </div>
-        </Show>
+        <Suspense fallback=|| ()>
+            <Show when=move || modal_status.get() fallback=move || ()>
+                <div class="fixed right-0 top-0 z-10 flex h-screen w-screen flex-col items-center justify-center drop-shadow-lg backdrop-blur-[2px] bg-[#00000055]">
+                    <div node_ref=modal_ref>{children.with_value(|children| children())}</div>
+                </div>
+            </Show>
         </Suspense>
-
-      }
+    }
 }

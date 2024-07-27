349dd2987ac17ee246a9505553de59d3d46b91c0
Esteban Borai <estebanborai@gmail.com>
2024-07-27 16:53:44 -0400

    feat: authentication

--- crates/server/src/handlers/mod.rs
+++ crates/server/src/handlers/mod.rs
@@ -1,1 +1,28 @@
+pub mod api;
 pub mod graphql;
+
+use std::sync::Arc;
+
+use axum::http::{header, HeaderValue, Method};
+use axum::routing::get;
+use axum::{Extension, Router};
+use tower_http::cors::CorsLayer;
+
+use crate::context::Context;
+use crate::graphql::schema::GraphQLSchema;
+
+pub fn router(context: Arc<Context>, schema: GraphQLSchema) -> Router {
+    let api = api::api();
+
+    Router::new()
+        .route("/graphql", get(graphql::playground).post(graphql::schema))
+        .nest("/api", api)
+        .layer(Extension(context))
+        .layer(Extension(schema))
+        .layer(
+            CorsLayer::new()
+                .allow_origin("*".parse::<HeaderValue>().unwrap())
+                .allow_headers([header::CONTENT_TYPE, header::AUTHORIZATION])
+                .allow_methods([Method::GET, Method::POST]),
+        )
+}

d75d9f0d97059f41102a6cd5a173dfb2544492ae
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-07-27 19:21:18 -0400

    feat(web): create the signup view (#207)

--- crates/web/src/components/layout/header.rs
+++ crates/web/src/components/layout/header.rs
@@ -1,24 +1,29 @@
-use leptos::{component, view, IntoView};
+use leptos::{component, create_rw_signal, view, IntoView, SignalSet};
 
-use crate::components::icons::home::Home;
+use crate::components::{auth::register::SignupCard, icons::home::Home, modal::Modal};
 
 #[component]
 pub fn Header() -> impl IntoView {
+    let is_open_auth_modal = create_rw_signal(false);
     view! {
-        <header class="sticky top-0 bg-white flex items-center justify-center shadow">
-            <div class="grid md:grid-cols-12 md:max-w-[1400px] px-4 py-2">
-                <figure class="md:col-start-1">
-                    <img src="https://via.placeholder.com/140x40" />
-                </figure>
-                <nav class="px-4 text-zinc-700 flex justify-start items-center md:col-start-4 md:col-end-10">
-                    <a class="flex justify-center items-center h-8 w-8" href="/">
-                        <Home class="h-6 w-6" />
-                    </a>
-                </nav>
-                <div class="md:col-start-12 md:col-end-13">
-                    User
+            <header class="sticky top-0 bg-white flex items-center justify-center shadow">
+                <div class="grid md:grid-cols-12 md:max-w-[1400px] px-4 py-2">
+                    <figure class="md:col-start-1"> <img src="https://via.placeholder.com/140x40" />
+                    </figure>
+                    <nav class="px-4 text-zinc-700 flex justify-start items-center md:col-start-4 md:col-end-10">
+                        <a class="flex justify-center items-center h-8 w-8" href="/">
+                            <Home class="h-6 w-6" />
+                        </a>
+                    </nav>
+                    <button on:click=move |_| {
+                        is_open_auth_modal.set(true);
+                      } class="md:col-start-12 md:col-end-13">
+                        User
+                    </button>
                 </div>
-            </div>
-        </header>
+            </header>
+            <Modal modal_status=is_open_auth_modal>
+                <SignupCard />
+            </Modal >
     }
 }
--- crates/web/src/components/mod.rs
+++ crates/web/src/components/mod.rs
@@ -1,8 +1,10 @@
 mod helper;
 
+pub mod auth;
 pub mod button;
 pub mod feed;
 pub mod icons;
 pub mod layout;
+pub mod modal;
 pub mod publisher;
 pub mod text_field;

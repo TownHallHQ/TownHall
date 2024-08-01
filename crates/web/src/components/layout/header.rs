eba336246c60bbc2f99d7039b54c8fa357022149
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-08-01 15:03:47 -0400

    chore: use the login component

--- crates/web/src/components/layout/header.rs
+++ crates/web/src/components/layout/header.rs
@@ -1,10 +1,16 @@
-use leptos::{component, create_rw_signal, view, IntoView, SignalSet};
+use leptos::{component, create_rw_signal, view, IntoView, Show, SignalGet, SignalSet};
 
-use crate::components::{auth::register::SignupCard, icons::home::Home, modal::Modal};
+use crate::components::{
+    auth::{login::LoginCard, register::SignupCard},
+    icons::home::Home,
+    modal::Modal,
+};
 
 #[component]
 pub fn Header() -> impl IntoView {
-    let is_open_auth_modal = create_rw_signal(false);
+    let is_open_auth_signup_modal = create_rw_signal(false);
+    let is_open_auth_login_modal = create_rw_signal(false);
+
     view! {
             <header class="sticky top-0 bg-white flex items-center justify-center shadow">
                 <div class="grid md:grid-cols-12 md:max-w-[1400px] px-4 py-2">
@@ -16,14 +22,22 @@ pub fn Header() -> impl IntoView {
                         </a>
                     </nav>
                     <button on:click=move |_| {
-                        is_open_auth_modal.set(true);
+                        is_open_auth_login_modal.set(true);
                       } class="md:col-start-12 md:col-end-13">
                         User
                     </button>
                 </div>
             </header>
-            <Modal modal_status=is_open_auth_modal>
+            <Modal modal_status=is_open_auth_signup_modal>
+                <Show when=move || is_open_auth_signup_modal.get() fallback=move || ()>
                 <SignupCard />
-            </Modal >
+                </Show>
+                </Modal >
+                <Modal modal_status=is_open_auth_login_modal >
+
+                <Show when=move || is_open_auth_login_modal.get() fallback=move || ()>
+                <LoginCard login_status=is_open_auth_login_modal signup_status=is_open_auth_signup_modal/>
+                </Show>
+                </Modal>
     }
 }

ebb8359ba98ba81cef511670e5eb94061918e65f
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-09-22 12:33:46 -0400

    chore!: posts query (#222)

--- crates/web/src/components/feed/mod.rs
+++ crates/web/src/components/feed/mod.rs
@@ -1,11 +1,28 @@
 mod post;
 
-use leptos::{component, view, IntoView};
+use leptos::{component, create_resource, view, IntoView};
+use townhall_client::Client;
 
 use self::post::Post;
 
 #[component]
 pub fn Feed() -> impl IntoView {
+    let posts = create_resource(
+        || (),
+        |_| async move {
+            let client = Client::new("http://localhost:8080");
+
+            let posts = client
+                .unwrap()
+                .post
+                .posts(None, None, Some(10), None)
+                .await
+                .unwrap();
+
+            posts
+        },
+    );
+
     view! {
         <div id="feed">
             <ul class="space-y-4">

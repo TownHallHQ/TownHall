11f376141c6730950cb046d13a5e7b781d8624f9
Esteban Borai <estebanborai@gmail.com>
2024-09-22 14:29:09 -0300

    feat(web): create and list posts (#227)

--- crates/web/src/components/publisher/mod.rs
+++ crates/web/src/components/publisher/mod.rs
@@ -1,10 +1,26 @@
-use leptos::{component, create_rw_signal, view, IntoView};
+use leptos::{component, create_action, create_rw_signal, view, IntoView, SignalGet};
+use townhall_client::{post::post_create::post_create::PostCreateInput, Client};
 
 use crate::components::text_field::TextField;
 
 #[component]
 pub fn Publisher() -> impl IntoView {
     let text_content = create_rw_signal(String::default());
+    let send_post_action = create_action(|content: &String| {
+        let content = content.to_owned();
+
+        async move {
+            Client::new("http://127.0.0.1:8080")
+                .unwrap()
+                .post
+                .post_create(PostCreateInput {
+                    title: content,
+                    content: None,
+                    parent_id: None,
+                })
+                .await
+        }
+    });
 
     view! {
         <div id="publisher" class="bg-white rounded-md p-4 flex items-start gap-4">
@@ -14,12 +30,16 @@ pub fn Publisher() -> impl IntoView {
                 </figure>
             </div>
             <div id="publisher-form" class="flex flex-col items-start justify-start w-full">
-                <form class="flex flex-col justify-start w-full">
+                <form class="flex flex-col justify-start w-full" on:submit=move |ev| {
+                            ev.prevent_default();
+                            let content = text_content.get();
+                            send_post_action.dispatch(content);
+                        }>
                     <div class="w-full h-12">
                         <TextField class="w-full" name="content" value=text_content />
                     </div>
                     <div class="flex justify-end items-center">
-                        <button>Post</button>
+                        <button type="submit">Post</button>
                     </div>
                 </form>
             </div>
